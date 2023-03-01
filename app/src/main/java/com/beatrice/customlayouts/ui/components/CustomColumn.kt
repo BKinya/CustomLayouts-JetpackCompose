d9ae4143ea63729980f6a6d9415ffb9962598d48
Beatrice Kinya <gitongakinya1@gmail.com>
2023-03-01 07:30:38 +0300

    failed attempt

--- app/src/main/java/com/beatrice/customlayouts/ui/components/CustomColumn.kt
+++ app/src/main/java/com/beatrice/customlayouts/ui/components/CustomColumn.kt
@@ -1,6 +1,6 @@
 package com.beatrice.customlayouts.ui.components
 
- androidx.compose.material.Surface
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -9,8 +9,7 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun CustomColumn(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
-  Layout(content = content, modifier = modifier)
-  { measurables, constraints ->
+  Layout(content = content, modifier = modifier) { measurables, constraints ->
     // MEASUREMENT SCOPE
     // Measurement scope
     var initialWidth = constraints.minWidth
@@ -20,7 +19,7 @@ fun CustomColumn(modifier: Modifier = Modifier, content: @Composable () -> Unit)
       initialWidth += 84
       it.measure(
         constraints.copy(
-          minWidth = if (initialWidth >= constraints.maxWidth) constraints.maxWidth else initialWidth
+          minWidth =  constraints.minWidth,
         )
       )
     }
--- app/src/main/java/com/beatrice/customlayouts/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/beatrice/customlayouts/ui/theme/HomeScreen.kt
@@ -1,11 +1,9 @@
 package com.beatrice.customlayouts.ui.theme
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,66 +14,68 @@ import com.beatrice.customlayouts.ui.components.CustomColumn
 @Composable
 fun HomeScreen() {
   CustomColumn(modifier = Modifier.padding(8.dp)) {
+    Text(text = "Beatrice")
+    Text(text = "Kinya")
     Box(modifier = Modifier
       .width(42.dp)
-      .height(20.dp)
+      .height(10.dp)
       .background(color = Color.Cyan))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(Color.DarkGray))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(Color.Magenta))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(color = Color.Cyan))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(Color.DarkGray))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(Color.Magenta))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(color = Color.Cyan))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(Color.DarkGray))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(Color.Magenta))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(color = Color.Cyan))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(Color.DarkGray))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(Color.Magenta))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(color = Color.Cyan))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(Color.DarkGray))
-    Box(modifier = Modifier
-      .width(42.dp)
-      .height(20.dp)
-      .background(Color.Magenta))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(Color.DarkGray))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(Color.Magenta))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(color = Color.Cyan))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(Color.DarkGray))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(Color.Magenta))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(color = Color.Cyan))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(Color.DarkGray))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(Color.Magenta))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(color = Color.Cyan))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(Color.DarkGray))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(Color.Magenta))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(color = Color.Cyan))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(Color.DarkGray))
+//    Box(modifier = Modifier
+//      .width(42.dp)
+//      .height(20.dp)
+//      .background(Color.Magenta))
   }
 }
 
